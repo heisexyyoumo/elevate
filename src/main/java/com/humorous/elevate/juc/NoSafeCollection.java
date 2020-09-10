551dd310d7774679e6dbebb8093e27ccf8390b1c
humorous <heisexyyoumo@gmail.com>
2020-09-10 23:54:32 +0800

    集合的线程安全

---
+++ src/main/java/com/humorous/elevate/juc/NoSafeCollection.java
@@ -0,0 +1,44 @@
+package com.humorous.elevate.juc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @author humorous
+ * @create 2020/9/10
+ * @since 1.0.0
+ * 不安全的集合类 list
+ * 1.故障现象：java.util.ConcurrentModificationException
+ * 2.解决方法：
+ * -- List<String> list = new Vector();
+ * -- List<String> list = Collections.synchronizedList(new ArrayList<>());
+ * -- List<String> list = new CopyOnWriteArrayList<>();
+ *
+ * Set  线程不安全
+ * HashSet， 底层是HashMap,value为static final的Object对象
+ * 解决方法是：CopyOnWriteArraySet();
+ *
+ * Map  线程不安全
+ * 解决方法是：new ConcurrentHashMap<>();
+ */
+
+public class NoSafeCollection {
+
+    public static void main(String[] args) {
+        /*List<String> list = new ArrayList<>();*/  //Vector线程安全，ArrayList效率高
+        /*List<String> list = Collections.synchronizedList(new ArrayList<>());*/  //Collection:接口，Collections:工具类
+        List<String> list = new CopyOnWriteArrayList<>();  //写时复制技术
+
+        for (int i = 0; i < 3; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));  //substring的参数 [,)
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
